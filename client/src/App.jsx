3e05d2f0118e324e78b3de96b91d285bfabe3be2
Arghya <arghya2801@gmail.com>
2024-11-19 22:15:23 +0530

    Imported and added react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
+import Sidebar from './components/Sidebar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <Navbar />
       Hello
+      <Sidebar />
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
+// import Home from './components/Home.jsx'
+// import Admin from './components/Admin.jsx'
+// import Cart from './components/Cart.jsx'
+import ErrorPage from "./ErrorPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
