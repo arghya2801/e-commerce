69678253c6edc8c95f1a6eb41da6c905a0e92068
Arghya <arghya2801@gmail.com>
2024-11-19 21:53:48 +0530

    Built basic navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='flex justify-around items-center bg-purple-600 py-4 text-white'>
+
+      <div className='text-2xl font-semibold'>Navbar</div>
+      <ul className='flex gap-4'>
+        <li>Home</li>
+        <li>Admin</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
