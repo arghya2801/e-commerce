ea03ac96cca8df0ad41f3a46e17084344c220c70
Arghya <arghya2801@gmail.com>
2024-11-20 18:31:39 +0530

    Styled sidebar to a floating static

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Sidebar = () => {
     return (
         <>
-            <div>Sidebar</div>
+            <div className='bg-purple-300 px-8 w-60 py-6 m-4 fixed rounded-lg'>
+                <h2 className='text-2xl font-semibold py-4'>Categories</h2>
+                <ul>
+                    <li>Category 1</li>
+                    <li>Category 2</li>
+                    <li>Category 3</li>
+                </ul>
+            </div>
         </>
     )
 }
