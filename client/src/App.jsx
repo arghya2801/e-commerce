9f72725656cb5c7182f2fd33a06378147ec542d5
Arghya <arghya2801@gmail.com>
2024-11-20 19:01:32 +0530

    Implemented grid view for items added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import AddItem from './components/AddItem.jsx'
+import Item from './components/Item.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,12 +13,19 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <div className='flex justify-around flex-row'> */}
-        <Sidebar />
-        <div className='ml-80 py-4'>
-          <AddItem />
+      <Sidebar />
+      <div className='ml-80 py-4'>
+        <AddItem />
+        <div className='grid grid-cols-3 gap-4'>
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
         </div>
-      {/* </div> */}
+      </div>
     </>
   )
 }
---
+++ client/src/components/Item.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Item = () => {
+    return (
+        <>
+            <div className='border border-solid-2 p-6 bg-white'>
+                <h3 className='text-2xl font-semibold'>Item Name </h3>
+                <p>Item Price: Rs 100</p>
+                <p>Item Category: Category 1</p>
+                <p>Item Description: Description text goes here</p>
+                <button className='bg-purple-700 rounded-md text-white p-2 mt-2'>Add to Cart</button>
+            </div>
+        </>
+    )
+}
+
+export default Item
