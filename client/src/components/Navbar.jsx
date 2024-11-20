91049fadadbd896eeaf96cf5232d17216c2a16b4
Arghya <arghya2801@gmail.com>
2024-11-20 18:31:18 +0530

    Changed font size

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-      <div className='flex justify-around items-center bg-purple-600 py-4 text-white'>
+      <div className='flex justify-around items-center bg-purple-800 py-4 text-white'>
 
-        <div className='text-2xl font-semibold'>Navbar</div>
+        <div className='text-4xl font-semibold'>Navbar</div>
         <ul className='flex gap-4'>
           <li>Home</li>
           <li>Admin</li>
