fb04bdc8c8cb5c0f87e1e6812db44aea16486532
Arghya <arghya2801@gmail.com>
2024-12-06 21:01:15 +0530

    Imported and added cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import postgres from "./db/postgres.js"
+// import { getProducts, addProduct, deleteProduct, getCartProducts, addToCart, deleteFromCart } from './db/postgres.js';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors()); 
 
 app.get("/", (request, response) => {
     response.json({
@@ -13,13 +16,20 @@ app.get("/", (request, response) => {
     });
 })
 
-// console.log(postgres.getProducts);
-app.get("http://localhost:3000/products", postgres.getProducts);
-app.post("http://localhost:3000/products", postgres.addProduct);
-app.post("http://localhost:3000/products", postgres.deleteProduct);
-app.get("http://localhost:3000/cart", postgres.getCartProducts);
-app.post("http://localhost:3000/cart", postgres.addToCart);
-app.post("http://localhost:3000/cart", postgres.deleteFromCart);
+console.log(postgres.getProducts);
+app.get("/products", postgres.getProducts);
+app.post("/products", postgres.addProduct);
+app.delete("/products", postgres.deleteProduct);
+app.get("/cart", postgres.getCartProducts);
+app.post("/cart", postgres.addToCart);
+app.delete("/cart", postgres.deleteFromCart);
+
+// app.get("/products", getProducts);
+// app.post("/products", addProduct);
+// app.delete("/products/:id", deleteProduct);
+// app.get("/cart", getCartProducts);
+// app.post("/cart", addToCart);
+// app.delete("/cart/:id", deleteFromCart);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
