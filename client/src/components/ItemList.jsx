86c652fdba1757378afe1723eb8718131af31d4c
Arghya <arghya2801@gmail.com>
2024-12-06 21:02:14 +0530

    Misc changes

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -3,29 +3,34 @@ import { useState } from 'react';
 import Item from './Item.jsx'
 
 const ItemList = () => {
-    const [itemData, setItemData] = React.useState([]);
+  const [itemData, setItemData] = React.useState([]);
 
-    React.useEffect(() => {
-        fetch('/products')
-            .then(response => response.json())
-            .then(data => setItemData(data.data))
-            .catch(error => console.log(error));
-    }, []);
+  React.useEffect(() => {
+      fetch('/products')
+          .then(response => {
+              if (!response.ok) {
+                  throw new Error(`HTTP error! status: ${response.status}`);
+              }
+              return response.json();
+          })
+          .then(data => setItemData(data.data)) // Ensure data.data is valid
+          .catch(error => console.error('Fetch error:', error));
+  }, []);
 
-    return (
-        <>
-        {itemData.map((item, i) => (
-            <Item
-          key={`${item.name}-${i}`}
-          name={item.name}
-          price={item.price}
-          category={item.category}
-          description={item.description}
-          quantity={item.quantity}
-          />
-        ))}
+  return (
+      <>
+          {itemData.map((item, i) => (
+              <Item
+                  key={`${item.product_id}-${i}`}
+                  name={item.name}
+                  price={item.price}
+                  category={item.category}
+                  description={item.description}
+                  quantity={item.quantity}
+              />
+          ))}
       </>
-    );
+  );
 };
 
 export default ItemList
