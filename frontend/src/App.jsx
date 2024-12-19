3e1452340cfe015cb7fc2ffcd3755d077f0b37de
arghya_333 <arghya2801@gmail.com>
2024-12-19 21:34:33 +0530

    Login status moves user to homepage after signup/login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,33 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
 import Navbar from "./components/Navbar.jsx";
+import { useUserStore } from "./stores/useUserStore.js";
+import { use } from "react";
 
 function App() {
+  const {user, checkAuth} = useUserStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
   return (
     <>
       <div className="min-h-screen">
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to="/" />} />
+          <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/" />} />
         </Routes>
       </div>
+      <Toaster />
     </>
   );
 }
