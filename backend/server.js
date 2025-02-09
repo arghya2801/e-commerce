e9cdfa68658405268283aac506cf08031ae29396
arghya_333 <arghya2801@gmail.com>
2025-02-09 18:32:51 +0530

    Removed redundant code

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -27,14 +27,6 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
-
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
 
 app.listen(PORT, () => {
 	console.log("Server is running on http://localhost:" + PORT);
